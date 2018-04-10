00b76d8b1a3f82821b2575dbd204eb68d0684f91
Evan <evanmlongwood@gmail.com>
2018-04-09 21:56:14 -0700

    exercise 3 with floats

---
+++ python_ex3.py
@@ -0,0 +1,28 @@
+# prints a string.
+print "I will now count my chickens:"
+# prints a string and does addition and division.
+print "Hens", float(25 + 30/ 6)
+# prints a string and does subtraction, multiplication, and devides the left hand operand by the right hand operand and returns a remainder.
+print "Roosters", float(100 - 25 * 3 % 4)
+# prints a string
+print "Now I will count the eggs:"
+# prints addition, subtration, modulous operation, and division.
+print float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# prints a question
+print "Is it true that 3 + 2 < 5- 7?"
+# prints addtion and subtraction, with their answers being greater than or less than. True or false.
+print float(3 + 2 < 5 - 7)
+# prints a string, then addition.
+print "What is 3 + 2?", float(3 + 2)
+# prints subtraction, which leaves a negative number.
+print "What is 5 - 7?", float(5 - 7)
+# prints a string.
+print "Oh, that's why it's False."
+# prints a string
+print "How about some more."
+# prints a question, followed by a true false statement.
+print "Is it greater?", float(5) > float(-2)
+# prints a question, followed by a true false statement.
+print "Is it greater or equal?", float(5) >= float(-2)
+# prints a question, followed by a true false statement.
+print "Is it less or equal?", float(5) <= float(-2)
