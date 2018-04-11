c1ea05311dd3567a726ee4c37b8edd9236099ced
Evan <evanmlongwood@gmail.com>
2018-04-11 07:53:15 -0700

    print practice

---
+++ python_ex7.py
@@ -0,0 +1,38 @@
+# This prints a string
+print "Mary had a little lamb."
+# This prints a string and adds a string via %
+print "Its fleece was white as %s." % 'snow'
+# This prints a string
+print "And everywhere that Mary went."
+# This printed 10 . via multiplication. 
+print "." * 10 # what'd that do?
+# Variable
+end1 = "C"
+# Variable
+end2 = "h"
+# Variable
+end3 = "e"
+# Variable
+end4 = "e"
+# Variable
+end5 = "s"
+# Variable
+end6 = "e"
+# Variable
+end7 = "B"
+# Variable
+end8 = "u"
+# Variable
+end9 = "r"
+# Variable
+end10 = "g"
+# Variable
+end11 = "e"
+# Variable
+end12 = "r"
+
+# what that comma at the end. try removing it to see what happends
+
+#removing the comma will print them out on different lines instead of being on the same line. 
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
