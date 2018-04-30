d787a1a4f47e070f55b421379c53b878a82275bc
Evan <evanmlongwood@gmail.com>
2018-04-30 14:29:36 -0700

    Taking a break

--- python_ex26.py
+++ python_ex26.py
@@ -64,7 +64,7 @@ def secret_formula(started):
 
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
@@ -72,7 +72,7 @@ print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont)
+print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
 
 
 sentence = "All good things come to those who wait."
@@ -85,8 +85,8 @@ print_last_word(words)
 print_first_word(sorted_words)
 print_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
+print sorted_words
 
-print_irst_and_last(sentence)
+print first_and_last(sentence)
 
-   print_first_a_last_sorted(senence)
+print first_and_last_sorted(sentence)
