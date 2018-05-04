859fb0f4dea302b6f5834183c98643840e3cbded
Evan <evanmlongwood@gmail.com>
2018-05-04 13:16:09 -0700

    added some notes.

--- python_ex30.py
+++ python_ex30.py
@@ -1,15 +1,18 @@
+# These are variables!
 people = 40
 cars = 40
 buses = 40
 
-
+# If cars are greater than people, print this sentence.
 if cars > people:
     print "We should take the cars."
+# Else, If cars are less than people, print this sentence.
 elif cars < people:
     print "We should not take the cars."
+# Otherwise, print this if it doesn't meet the first two criteria. 
 else:
     print "We can't decide."
-
+# Same as stated above. 
 if buses > cars:
     print "That's too many buses."
 elif buses < cars:
@@ -21,3 +24,5 @@ if people > buses:
     print "Alright, let's just take busses."
 else:
     print "Fine, let's just stay home then."
+
+
