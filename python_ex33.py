014098c20504936f2f3bd04f298f9392f0aa631b
Evan <evanmlongwood@gmail.com>
2018-05-07 14:09:19 -0700

    variable made while loop

--- python_ex33.py
+++ python_ex33.py
@@ -13,3 +13,21 @@ print "The numbers: "
 
 for num in numbers:
     print num
+
+def while_loop(limit, increment):
+    i = 0
+    numbers = []
+
+    while i < limit:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i = i + increment 
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+    print "The numbers: "
+    for num in numbers:
+        print num
+
+while_loop(6, 1)
