bbb9a76fad3de7206ade2e1496dda9f69e01eb97
Evan <evanmlongwood@gmail.com>
2018-05-07 14:17:32 -0700

    for loop variable

--- python_ex33.py
+++ python_ex33.py
@@ -13,7 +13,7 @@ print "The numbers: "
 
 for num in numbers:
     print num
-
+# while loop variable
 def while_loop(limit, increment):
     i = 0
     numbers = []
@@ -31,3 +31,14 @@ def while_loop(limit, increment):
         print num
 
 while_loop(6, 1)
+# for loop variable
+def for_loop(limit, increment):
+    for i in range(0, limit, increment):
+        print "At the top i is %d" % i
+        print "Numbers now: ", range(0, i + increment, increment)
+        print "At the bottom i is %d" % (i + increment)
+    print "The numbers: "
+    for i in range(0, limit, increment):
+        print i
+
+for_loop(6, 1)
