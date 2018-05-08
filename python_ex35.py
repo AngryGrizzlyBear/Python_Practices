232e04966ac525361e01195635f56a170e2ee670
Evan <evanmlongwood@gmail.com>
2018-05-08 13:10:44 -0700

    bear roo

--- python_ex35.py
+++ python_ex35.py
@@ -14,5 +14,27 @@ def gold_room():
         exit(0)
     else:
         dead("You greedy bastard!")
+# Dead isn't defined yet. Otherwise, anything less than 5o works.   
 
-gold_room()
+def bear_room():
+    print "There is a bear in here."
+    print "The bear has a bunch of honey."
+    print "The fat bear is in front of another door."
+    print "How are you going to move the bear?"
+    bear_moved = False
+
+    while True:
+        next = raw_input("> ")
+
+        if next == "take honey":
+            dead("The bear lools at you then slaps your face off.")
+        elif next == "taunt the bear" and not bear_moved:
+            print "The bear has moved from the door. You can go through it now."
+            bear_moved = True
+        elif next == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews off your leg off.")
+        elif next == "open door" and bear_moved:
+            gold_room()
+        else:
+            print "I got no idea what that means."
+bear_room()
