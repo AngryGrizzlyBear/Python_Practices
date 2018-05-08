b86dc32e4fbd70ab86a16a28939a78ade1d57a88
Evan <evanmlongwood@gmail.com>
2018-05-08 10:25:55 -0700

    accessing lists

---
+++ python_ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at one is bear" 
+print "The third animal is %r" % animals[2]
+print "The first animal is %r" % animals[0]
+print "The animal at 3 is peacock"
+print "The fifth animal is %r" % animals[4]
+print "The animal at 2 is python."
+print "The sixth animal is %r" % animals[5]
+print "The animal at 4 is kangaroo."
