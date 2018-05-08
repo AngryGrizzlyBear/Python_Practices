bbe9cad9483d223ce95f165aeb7208e66d233e45
Evan <evanmlongwood@gmail.com>
2018-05-08 12:40:01 -0700

    First Part of the problem

---
+++ python_ex35.py
@@ -0,0 +1,18 @@
+from sys import exit
+
+def gold_room():
+    print "This room is full of gold. How much will you take?"
+
+    next = raw_input("> ")
+    if "0" in next or "1" in next:
+        how_much = int(next)
+    else:
+        dead("Man, learn to type a number.")
+
+    if how_much < 50:
+        print "Nice, you're not greedy, you win!"
+        exit(0)
+    else:
+        dead("You greedy bastard!")
+
+gold_room()
