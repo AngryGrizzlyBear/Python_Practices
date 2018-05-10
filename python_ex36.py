65a25e83f4474f13b93caa65e2b89069926e6fcd
Evan <evanmlongwood@gmail.com>
2018-05-10 09:11:37 -0700

    The Beginning of my own game.

---
+++ python_ex36.py
@@ -0,0 +1,9 @@
+from sys import exit
+
+def start():
+    print "You're traveling down a road."
+    print "You come across three paths."
+    print "You have three choices. Which Path do you take?"
+    print "Please select your answer.\nA. Right Road.\nB. Middle Road.\nC. Left Road."
+
+start()
