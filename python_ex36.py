ae8247b3a741d7b94ee4b18deb728a5992a7de23
Evan <evanmlongwood@gmail.com>
2018-05-10 16:37:50 -0700

    Created Options.

--- python_ex36.py
+++ python_ex36.py
@@ -1,3 +1,6 @@
+# The goal of this game is to keep you from progressing. 
+# Don't be afraid! Otherwise, you're going home.
+
 from sys import exit
 
 def start():
@@ -5,5 +8,19 @@ def start():
     print "You come across three paths."
     print "You have three choices. Which Path do you take?"
     print "Please select your answer.\nA. Right Road.\nB. Middle Road.\nC. Left Road."
+
+    next = raw_input("> ")
+
+    if next == "A".lower():
+        print "You have chosen to go down the right road."
+    elif next == "B".lower():
+        print "You have chosen to go down the middle road."
+    elif next == "C".lower():
+        print "You have chosen to down the left road."
+    else: 
+        game_over()
+
+def game_over():
+    print "You're too scared to progress. You turn around and go home."
 
 start()
