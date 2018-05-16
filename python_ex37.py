68f604bfbe28e4d5d57225b0e268b79b7e914d0c
Evan <evanmlongwood@gmail.com>
2018-05-16 14:29:11 -0700

    word usage

---
+++ python_ex37.py
@@ -0,0 +1,33 @@
+#Symbol Review
+
+# KEYWORDS
+
+# and 
+# del
+# from 
+# not
+# while 
+# as 
+# elif globalsor 
+# with
+# assert
+# else
+# if 
+# pass 
+# yield 
+# break 
+# except
+# import 
+# print 
+# class 
+# exec 
+# in 
+# raise 
+# continue 
+# finally 
+# is 
+# return 
+# def 
+# for 
+# labda 
+# try
