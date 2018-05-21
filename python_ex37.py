b4451171becad29889226209ecc6ed427f925d36
Evan Longwood <evanmlongwood@gmail.com>
2018-05-20 19:48:58 -0700

    Updated my learning sheet.

--- python_ex37.py
+++ python_ex37.py
@@ -2,31 +2,34 @@
 
 # KEYWORDS
 
-# and 
-# del
-# from 
-# not
-# while 
+# and is like saying this and this.
+# del deletes whatever it is you assign.
+# For example: del huge_list.
+# from you're assigning python to import features FROM designated location.
+# not makes a statement true, I suppose. Depending on how you use it.
+# while This is how infinite loops are made!
 # as 
-# elif globalsor 
+# elif else if
+# global
+# or truth/boolean statements.
 # with
 # assert
-# else
-# if 
+# else if this doesn't work, else this.
+# if Tupac didn't die, print something
 # pass 
 # yield 
 # break 
 # except
-# import 
-# print 
+# import a file or function
+# print a string or an answer
 # class 
 # exec 
 # in 
 # raise 
 # continue 
 # finally 
-# is 
-# return 
+# is
+# return the answer back to the function.
 # def 
 # for 
 # labda 
