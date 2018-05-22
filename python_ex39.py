da5b892e45096c5fe24d29d5c4692704eab9e5ff
Evan Longwood <evanmlongwood@gmail.com>
2018-05-21 23:02:15 -0700

    Will finish up the rest tomorrow

---
+++ python_ex39.py
@@ -0,0 +1,18 @@
+# create a amapping of state to abbreviation
+states = [
+    'Oregon':  'OR',
+    'Florida':  'FL',
+    'California': 'CA,'
+    'New York': 'NY',
+    'Michigan': 'MI',
+]
+
+# creates a basic set of states and some cities in them
+cities = [
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+]
+
+# add some more cities
+cities['NY'] = 'New York'
