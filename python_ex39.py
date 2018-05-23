e6254287546543a2350829ab9ac561ecd3ba3446
Evan Longwood <evanmlongwood@gmail.com>
2018-05-22 22:47:01 -0700

    Added NY and OR

--- python_ex39.py
+++ python_ex39.py
@@ -16,3 +16,11 @@ cities = {
 
 # add some more cities
 cities['NY'] = 'New York'
+cities["OR"] = 'Portland'
+
+# print out some cities
+print '-' * 10
+print "NY State has: ", cities['NY']
+print "OR State has: ", cities['OR']
+
+
