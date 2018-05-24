d58ac37ec577e016e1c327841a3255b0d68f514c
Evan Longwood <evanmlongwood@gmail.com>
2018-05-23 21:15:15 -0700

    Making a new dictionary for practice.

---
+++ python_ex39self.py
@@ -0,0 +1,18 @@
+states = {
+    'Washington': 'WA',
+    'Wyoming': 'WY' ,
+    'Nevada': 'NV',
+    'Montana': 'MT'
+}
+
+cities = {
+    'WA': 'Seattle',
+    'NV': 'Carson City',
+    'MT': 'Helena'
+}
+
+cities['WY'] = 'Cheyenne'
+
+print '-' * 10
+print "WY State has: ", cities['WY']
+
