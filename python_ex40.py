084f26cc15e490536322a0d55b0c3682ed6a97ea
Evan Longwood <evanmlongwood@gmail.com>
2018-05-24 22:31:24 -0700

    added a song.

--- python_ex40.py
+++ python_ex40.py
@@ -12,8 +12,15 @@ happy_bday = Song(["Happy birthday to you",
                    "So I'll stop right there\n"])
 
 bulls_on_parade = Song(["They rally around the family",
-                        "With pockets full of shells"])
+                        "With pockets full of shells\n"])
+
+black_parade = Song(["When I was",
+                     "A young boy",
+                     "My father took me into the city",
+                     "to see a marching band."])
 
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
+
+black_parade.sing_me_a_song()
