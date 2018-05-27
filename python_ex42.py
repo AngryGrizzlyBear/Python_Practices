dc8f0ef2dad070a5d0de6a57e604995e7a9332d8
Evan Longwood <evanmlongwood@gmail.com>
2018-05-27 15:01:22 -0700

    Fixed it (I think.) This project is really complicated.

---
+++ python_ex42.py
@@ -0,0 +1,5 @@
+# Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
+
+# I've decided that I'm going to move on along and start porgramming in Python 3. Maybe I'll come back to this later.
