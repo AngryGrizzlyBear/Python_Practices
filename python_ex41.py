7697569c2cd49ec877ffc972dff6b80a3bfa1e25
Evan Longwood <evanmlongwood@gmail.com>
2018-05-27 11:26:14 -0700

    Fixed it (I think.) This project is really complicated.

--- python_ex41.py
+++ python_ex41.py
@@ -21,7 +21,7 @@ PHRASES = {
 }
 
 # Do they want to drill phases first
-PRHASE_FIRST = False
+PHRASE_FIRST = False
 if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 
@@ -37,7 +37,7 @@ def convert(snippet, phrase):
     param_names = []
 
     for i in range(0, snippet.count("@@@")):
-        param_count = random.randint(1,3)
+        param_count = random.randint(1, 3)
         param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
@@ -45,16 +45,16 @@ def convert(snippet, phrase):
 
         # fake class names
 
-    for word in class_names:
-        result = result. replace("%%%", word, 1)
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
 
     # fake other names
-    for word in other_names:
-        result = result.replace("***", word, 1)
+        for word in other_names:
+            result = result.replace("***", word, 1)
 
-    #fake parameter lits
-    for word in param_names:
-        result = result.replace("@@@", word, 1)
+    # fake parameter lits
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
 
         results.append(result)
 
